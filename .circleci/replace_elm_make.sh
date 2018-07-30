d56a6c37b824ee2e9a21d256810b79b29e2868dd
ymtszw <ymtszw@gmail.com>
2018-07-31 01:38:56 +0900

    [master] Update path to bin

--- .circleci/replace_elm_make.sh
+++ .circleci/replace_elm_make.sh
@@ -4,15 +4,15 @@
 # Using rather unsafe `pwd` since $HOME is unavailable in CircleCI
 set -euo pipefail
 ncore=${1:-1}
-if ! grep "sysconfcpus -n ${ncore}" "$(npm config get prefix)/elm-make"; then
-  if [ ! -f "$(npm config get prefix)/elm-make-old" ]; then
-    mv "$(npm config get prefix)/elm-make" "$(npm config get prefix)/elm-make-old"
+if ! grep "sysconfcpus -n ${ncore}" "$(npm config get prefix)/bin/elm-make"; then
+  if [ ! -f "$(npm config get prefix)/bin/elm-make-old" ]; then
+    mv "$(npm config get prefix)/bin/elm-make" "$(npm config get prefix)/bin/elm-make-old"
   fi
-  cat << EOF > "$(npm config get prefix)/elm-make"
+  cat << EOF > "$(npm config get prefix)/bin/elm-make"
 #!/usr/bin/env bash
 set -eu
 echo "Running elm-make with sysconfcpus -n ${ncore}"
-$(pwd)/sysconfcpus/bin/sysconfcpus -n ${ncore} "$(npm config get prefix)/elm-make-old" "\$@"
+$(pwd)/sysconfcpus/bin/sysconfcpus -n ${ncore} "$(npm config get prefix)/bin/elm-make-old" "\$@"
 EOF
-  chmod +x "$(npm config get prefix)/elm-make"
+  chmod +x "$(npm config get prefix)/bin/elm-make"
 fi
